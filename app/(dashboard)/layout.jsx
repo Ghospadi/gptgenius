d960b013681337008e2fc13300560e915f5e05b3
Ghospadi <92799055+Ghospadi@users.noreply.github.com>
2024-04-29 12:38:04 +0300

    init with clark

---
+++ app/(dashboard)/layout.jsx
@@ -0,0 +1,30 @@
+import SideBar from "@/components/SideBar";
+import {FaBarsStaggered} from "react-icons/fa6";
+
+export const metadata = {
+  title: "GPTGenius",
+  description:
+    "GPTGenius is a web app that allows users to chat with OpenAI's GPT-4.",
+};
+
+const layout = ({children}) => {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input type="checkbox" id="my-drawer-2" className="drawer-toggle"/>
+      <div className="drawer-content">
+        <label htmlFor="my-drawer-2" className="drawer-button lg:hidden fixed top-6 right-6">
+          <FaBarsStaggered className="w-8 h-8 text-primary"/>
+        </label>
+        <div className="bg-base-200 px-8 py-12 min-h-screen">
+          {children}
+        </div>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+        <SideBar/>
+      </div>
+    </div>
+  );
+};
+
+export default layout;
---
+++ app/(dashboard)/tours/page.jsx
@@ -0,0 +1,11 @@
+function ToursPage() {
+  return (
+    <div>
+      <h1>
+        Tours Page
+      </h1>
+    </div>
+  );
+}
+
+export default ToursPage;
